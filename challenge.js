95339f969aa629f3e7146d7c9a7e58fa2c2d9814
siham adam <sihamfarhan90@gmail.com>
2023-10-22 21:59:57 +0300

    i completed the task

--- challenge.js
+++ challenge.js
@@ -1,2 +1,27 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+function calculator(number1,number2,operator){
+    if(operator =="+"){
+        return`${number1+number2}`
+    }else if (operator =="-") {
+        return`${number1-number2}`
+    }else if (operator =="*") {
+        return`${number1*number2}`
+    }else if (operator =="/") {
+        return`${number1/number2}`
+    }else if (operator =="%") {
+        return`Invalid operator`
+    }
+    else {
+        return`there is no operator`
+    }
+}
+addition = calculator(2,3,"+");
+console.log(addition)
+substraction= calculator(5,3,"-");
+console.log(substraction)
+multiplication = calculator(2,3,"*");
+console.log(multiplication)
+division = calculator(3,3,"/");
+console.log(division)
+out_of = calculator(3,3,"%");
+console.log(out_of)
+
